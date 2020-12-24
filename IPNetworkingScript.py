1ecdcb6688690a550692f4a04543e862ecd3075b
AkumaSaru <armond.domina@gmail.com>
2020-12-24 02:44:39 -0500

    update

--- IPNetworkingScript.py
+++ IPNetworkingScript.py
@@ -6,8 +6,8 @@ IPNetworkingScript - Created By Ace & Akuma
 '''
 import subprocess
 import ipaddress
-# supply IP Address + Subnet (255.255.255.252) or CID Notation /24
 import time
+# supply IP Address + Subnet (255.255.255.252) or CID Notation /24
 
 user_input = input( "Please enter a Private IP address range: \n examples: \n 10.0.0.0 - 10.255.255.255 \n 172.16.0.0 - 172.31.255.255 \n 192.168.0.0 - 192.168.255.255 \n\n IP Address Range: \t" )
 print( " You entered: \t\t" + user_input + "\n\n")
